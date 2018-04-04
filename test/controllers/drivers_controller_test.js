d96790fc5dae6d8cc733a29018b1812ff030ef04
puchk <kpuchalski@gmail.com>
2018-04-04 03:45:48 -0700

    tests driver edits

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -18,4 +18,20 @@ describe('Drivers controller', () => {
 				});
 		});
 	});
+
+	it('PUT to /api/drivers/id edits an existing driver', done => {
+		const driver = new Driver({ email: 'new@test.com', driving: false });
+		driver.save().then(() => {
+			request(app)
+				.put('/api/drivers/' + driver._id)
+				.send({ driving: true })
+				.end(() => {
+					Driver.findOne({ email: 'new@test.com'})
+						.then(driver => {
+							assert(driver.driving === true);
+							done();
+					});
+				});
+		})
+	});
 });

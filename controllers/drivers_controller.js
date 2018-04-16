4527432d852e01ed260c76c1065edab3679a37e6
puchk <kpuchalski@gmail.com>
2018-04-15 23:39:09 -0700

    tests geoqueries

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -7,18 +7,18 @@ module.exports = {
 	},
 
 	index(req, res, next) {
-        const { lng, lat } = req.query;
-        
-        Driver.aggregate([
-            {
-                '$geoNear':{
-                    "near": {'type': 'Point', 'coordinates': [parseFloat(lng), parseFloat(lat)]},
-                    "spherical": true, "distanceField": 'dist', "maxDistance": 200000
-                }
-            }    
-        ])
-                .then(drivers => res.send(drivers))
-                .catch(next);
+    const { lng, lat } = req.query;
+    
+    Driver.aggregate([
+      {
+        '$geoNear':{
+          "near": {'type': 'Point', 'coordinates': [parseFloat(lng), parseFloat(lat)]},
+          "spherical": true, "distanceField": 'dist', "maxDistance": 200000
+        }
+      }    
+    ])
+      .then(drivers => res.send(drivers))
+      .catch(next);
     },
 
 	create(req, res, next) {
--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -63,7 +63,8 @@ describe('Drivers controller', () => {
 				request(app)
 				.get('/api/drivers?lng=-80&lat=25')
 				.end((err, response) => {
-					console.log(response);
+					assert(response.body.length === 1);
+					assert(response.body[0].email === 'driver2@test.com');
 					done();
 				});
 			});
